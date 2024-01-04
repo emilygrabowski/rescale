dd2f316ec156931d070c969356a41343000f9457
emilygrabowski <emilygrabowski@users.noreply.github.com>
2024-01-04 15:57:30 -0800

    Checked it with typehint

--- test/test_rescale.py
+++ test/test_rescale.py
@@ -2,7 +2,7 @@ import numpy as np
 from rescale.rescale import rescale
 
 
-def test_rescale_range():
+def test_rescale_range() -> bool:
     i = np.linspace(0, 100, 5)
     o = rescale(i)
     return (o.min() == 0) & (o.max() == 1)
