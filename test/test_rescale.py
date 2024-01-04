f8f85364a730ebef22f0a934abd19d412ea15f23
emilygrabowski <emilygrabowski@users.noreply.github.com>
2024-01-04 14:04:20 -0800

    add test rescale

---
+++ test/test_rescale.py
@@ -0,0 +1,7 @@
+import numpy as np
+from rescale.rescale import rescale
+
+def test_rescale_range():
+    input = np.linspace(0, 100, 5)
+    output = rescale(input)
+    return((output.min()==0) & (output.max()==1))
